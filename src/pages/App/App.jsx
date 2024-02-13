75277d350e90d6d14c699078dc130b193862369f
btlenny <btlenny@gmail.com>
2024-02-12 16:36:56 -0800

    adding headlessui

--- src/pages/App/App.jsx
+++ src/pages/App/App.jsx
@@ -1,11 +1,13 @@
 import './App.css';
 import CalendarGrid from '../../components/CalendarGrid/CalendarGrid.jsx';
+import NavBar from '../../components/NavBar/NavBar.jsx';
 
 export default function App() {
   return (
     <main className="App">
       <h1>hueman</h1>
       <CalendarGrid />
+      <NavBar />
     </main>
   );
 }
