e43554f5006f17ce8c4fd5f0d42f6a650872476b
btlenny <btlenny@gmail.com>
2024-02-15 14:52:35 -0800

    tailwind update

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {
       fontFamily: {
-        work: ['Work Sans', 'sans-serif'],
+        work: ["Work Sans", "sans-serif"],
       },
     },
   },
   plugins: [],
-}
-
+};
